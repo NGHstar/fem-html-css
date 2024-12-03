a10366bf63b7424bd2d97442f9d43eb596fe2737
NGHSTAR <nghstarcc@gmail.com>
2024-12-03 15:15:45 +0330

    fix: carousel-hero

--- js/main.js
+++ js/main.js
@@ -6,3 +6,31 @@ headerNavToggleBtn.addEventListener("click", () => {
   headerNavigation.classList.toggle("expanded");
   header.classList.toggle("expanded");
 });
+
+// کد کاروسل
+const swiper = new Swiper(".carousel__card-wrapper", {
+  loop: false,
+  spaceBetween: 20,
+
+  slidesPerView: 1,
+  spaceBetween: 24,
+
+  // pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: { slidesPerView: 1 },
+    768: { slidesPerView: 2 },
+    1024: { slidesPerView: 3 },
+  },
+});
